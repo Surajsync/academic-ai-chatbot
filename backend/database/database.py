91e7e6efda6bcbc5dd62a9062ed7a95f132fc6a8
Surajsync <officialsuraj93@gmail.com>
2026-04-04 19:56:19 +0530

    Replaced print with logging for DB connection

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from backend.config import settings
+import logging
 
 DATABASE_URL = settings.DATABASE_URL
 
-print("Using DB:", settings.DATABASE_URL)
+logging.basicConfig(level=logging.INFO)
+logging.info("Database Connected")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
